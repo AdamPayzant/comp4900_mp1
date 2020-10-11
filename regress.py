568bdb7269743b1ef06c5a5c033a8a32233e0b87
AdamPayzant <payzantedwardiv@gmail.com>
2020-10-10 20:04:14 -0400

    Tentatively did the regression

--- regress.py
+++ regress.py
@@ -3,34 +3,43 @@ import pandas as pd
 import numpy as np
 import os
 
-class regress:
-    def __init__(self):
-        self.w = np.array([])
-        print("Initializing")
+class Regress:
+    # I hate this class and hope it will burn. I'm uncertain about everything in my implementation
+    def __init__(self, features):
+        self.w = np.zeros((1, features))
+        self.features = features
         
-    def fit(self, data, labels):
-        print("Fitting")
-        if(self.w.size == 0):
-            self.__estimateW(data)
-
-    def __estimateW(self, data):    
-        print("Estimating w")
+    def fit(self, data, labels, lr):
+        a = -lr
+        wk1 = np.zeros((1, self.features))
+        # TODO: FIX THE RANGE
+        # OTherwise I think this is right?
+        for _ in range(0,100):
+            a += lr
+            sum = 0
+            for i in range(0, self.features):
+                sum += data[i] * (labels[i] - (1 / (1 + math.exp(- np.dot(self.w.T, data[i])))))
+            wk1 = self.w - a * sum
+            # TODO: CONDITIONAL FOR THE END CASE
+            # w_k+1 - w_k || < e
+            #     end
+            self.w = wk1
 
     def predict(self, x):
-        if(self.w.size == 0):
-            return None
-        if(__p0(data) > __p1(data)):
+        # Compares P(x=1 | Y) and P(x=0 | Y)
+        if(self.__p0(x) > self.__p1(x)):
             return 0
         else:
             return 1
 
     def __p0(self, x):
-        return -1/(1 + math.exp(- np.dot(w.T, x)))
-
+        # Finds P(x=0 | Y)
+        return -1/(1 + math.exp(- np.dot(self.w.T, x)))
     def __p1(self, x):
-        return 1/(1 + math.exp(- np.dot(w.T, x)))
-
+        # Finds P(x=1 | Y)
+        return 1/(1 + math.exp(- np.dot(self.w.T, x)))
     def reset(self):
+        # Resets the model, I don't know why you would need this, but it's there?
         self.w = np.array([])
 
 
