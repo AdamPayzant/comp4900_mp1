6bc5b2054c3035ec99bc37823c5967bb42777991
Alexander Cornish <46353834+CornUkOpia@users.noreply.github.com>
2020-10-12 19:16:21 -0400

    Update Kfold.py

--- Kfold.py
+++ Kfold.py
@@ -35,6 +35,7 @@ def loadCSV(filename):
     newPath = os.path.join(path, filename)
     return pd.read_csv(newPath)
 
+
 df = loadCSV("hepatitis.csv")
 kFoldData = KFold()
 shuffled = kFoldData.shuffle(df)
@@ -43,7 +44,7 @@ for x in range(0, kFoldData.k):
     trainingSet, validationSet = kFoldData.splitAtX(kFoldData.k,x,shuffled)
     trainingSetData = trainingSet.iloc[:,:-1]
     trainingSetLabel = trainingSet.iloc[:,-1:]
-    linReg.fit(trainingSetData.to_numpy(),trainingSetLabel.to_numpy(),0.1,len(trainingSetData))
+    linReg.fit(trainingSetData.to_numpy().tolist(),trainingSetLabel.to_numpy().tolist(),0.1,len(trainingSetData))
     validationSetData = validationSet.iloc[:,:-1]
     validationSetLabel = validationSet.iloc[:,-1:]
     validationSetLabel = validationSetLabel.to_numpy()
