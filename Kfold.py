09b7e98ad6f1229cc9b18b0a54525c89350b4069
Alexander Cornish <46353834+CornUkOpia@users.noreply.github.com>
2020-10-12 16:12:30 -0400

    Update Kfold.py

--- Kfold.py
+++ Kfold.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 import copy
+import regress
 
 class KFold:
     def __init__(self, k=10):
@@ -25,3 +26,16 @@ def loadCSV(filename):
     path = str(os.path.dirname(os.path.realpath(__file__)))      
     newPath = os.path.join(path, filename)
     return pd.read_csv(newPath)
+
+df = loadCSV("hepatitis.csv")
+kFoldData = KFold()
+shuffled = kFoldData.shuffle(df)
+linReg = regress.Regress(len(df.iloc[:,:-1].columns))
+for x in range(0, kFoldData.k):
+    trainingSet, validationSet = kFoldData.splitAtX(kFoldData.k,x,shuffled)
+    trainingSetData = trainingSet.iloc[:,:-1]
+
+    trainingSetLabel = trainingSet.iloc[:,-1:]
+    linReg.fit(trainingSetData.to_numpy(),trainingSetLabel.to_numpy(),0.1,len(trainingSetData))
+    validationSetData = validationSet.iloc[:,:-1]
+    validationSetLabel = validationSet.iloc[:,-1:]
